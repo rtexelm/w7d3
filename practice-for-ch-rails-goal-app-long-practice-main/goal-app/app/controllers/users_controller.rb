8e45bb2a8f4f2857b1108954064c71ced68d0b8d
jkhbuild <jkh.build@gmail.com>
2022-10-26 15:27:07 -0400

    views started

--- practice-for-ch-rails-goal-app-long-practice-main/goal-app/app/controllers/users_controller.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal-app/app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def new
-    redner :new
+    render :new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url
+      redirect_to user_url(@user)
     else
       render :new
     end
